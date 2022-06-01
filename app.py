9bf7f62d308d7216b04e184039d60e6017bd779a
williamjamesj <61896069+williamjamesj@users.noreply.github.com>
2022-06-01 22:32:47 +1000

    token based authentication

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from music import retriveSong, getChartLyrics
 from passlib.hash import sha256_crypt
 from dotenv import load_dotenv
 import time
+import secrets
 import pyotp
 
 load_dotenv()
@@ -23,8 +24,8 @@ def index():
 
 @app.route("/login", methods=["GET","POST"])
 def login(): # This login is used when the user does not already have an issued token.
-    if request.method == "POST": # The react mobile app will send the user's credentials as a JSON object over POST.
-        credentials = request.get_json()
+    if request.method == "POST": # The react mobile app will send the user's credentials as a JSON object over POST. 
+        credentials = request.get_json() # Doesn't use a form, uses JSON.
         email = credentials["email"]
         password = credentials["password"]
         print(request.get_json())
@@ -36,15 +37,33 @@ def login(): # This login is used when the user does not already have an issued 
                 session["tempUserID"] = userID # Temporarily assign the userID to the session, which still prevents any use, but allows the user to proceed to the 2fa challenge.
                 return jsonify({"status": "2fa"})
             else:
+                token = issue_token(userID)
                 name = userDetails[0]["name"]
                 session["userID"] = userID
-                return(jsonify({"status":"authenticated", "userID":userID, "name":name}))
+                return(jsonify({"status":"authenticated", "userID":userID, "token":token}))
         else:
             return(jsonify({"status":"Invalid Credentials"}))
     return jsonify({})
 
+def issue_token(userID):
+    token = secrets.token_urlsafe(32)
+    yearOfSeconds = 60*60*24*365 # 1 year in seconds, the default expiry for a user's token.
+    DATABASE.ModifyQuery("INSERT INTO tokens (userID, token, expiry) VALUES (?, ?, ?)", (userID, token, time.time() + yearOfSeconds))
+    return token # The client recieves the authentication token.
+    
+@app.route("/token_login", methods=["GET","POST"])
+def tokenLogin(): # This login is used when the user already has an issued token.
+    time.sleep(5)
+    if request.method == "POST":
+        token = request.get_json()["token"]
+        userDetails = DATABASE.ViewQuery("SELECT * FROM users INNER JOIN tokens on users.userID = tokens.userID WHERE tokens.token = ?", (token,))
+        if userDetails: # If the token is valid, the user's details can be found.
+            session["userID"] = userDetails[0]["userID"]
+            return(jsonify({"status":"authenticated"}))
+    return(jsonify({"status":"Invalid Token"}))
+
 @app.route("/2fa", methods=["GET","POST"])
-def twofactor():
+def twoFactor():
     if request.method == "POST":
         code = request.get_json()["code"]
         userID = session["tempUserID"]
@@ -53,7 +72,8 @@ def twofactor():
         print(code)
         if pyotp.TOTP(OTPCode).verify(code):
             session["userID"] = userID
-            return(jsonify({"status":"authenticated"}))
+            token = issue_token(userID)
+            return(jsonify({"status":"authenticated", "token":token}))
         else:
             return(jsonify({"status":"Invalid Code"}))
     return jsonify({})
@@ -63,4 +83,4 @@ def twofactor():
 # print(getLyrics("", ""))
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
